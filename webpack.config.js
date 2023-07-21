5e573d27531930220b2244501831c04f44dfe458
Bruno Ferreira <brunomgferreira17@gmail.com>
2023-07-21 12:27:44 +0100

    initial setup is done

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        assetModuleFilename: '[name][ext]',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules : [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+        ],
+    },
+}
